b23d5ea4aef3d7e111463304065b3b41ed29c5f4
vinay <vinaykotagiri123456@gmail.com>
2022-03-12 09:58:17 +0530

    Added fn headers

--- math2.py
+++ math2.py
@@ -1,8 +1,12 @@
+# add implementation
 def add(x,y):
 	pass
+# substarct  implementation
 def substract(x,y):
 	pass
+#multipy implementation
 def multiply(x,y):
 	pass
+#divid eimplementation
 def divide(x,y):
 	pass
